671d8c47d380723edc017831eaab64bcefb2ef30
Ram Sowmya Narayanan <rangamannar98@gmail.com>
2020-05-17 18:22:08 -0400

    Created new script

--- final_4.py
+++ final_4.py
@@ -13,10 +13,16 @@ import sys
 # TODO values we get twice the intended (?) do we divide it by 2?
 # TODO write comments in the code and add description to functions
 # TODO change the variables names vastly
-# TODO -- code is taking too long find ways to make this faster
 # TODO change the boroughs to what we used in the file "TEST-BDM.ipynb"
 
 def clean_violations(violations):
+	'''
+	PREPROCESSING:
+	This function takes in the violations (all five files) as input. 
+	In this function, we clean the violations table and drop the unwanted columns.
+	We split house number into two to accomadate for compound house numbers
+	We return: the violations file after pivoting on the years to reduce number of rows
+	'''
     
     violations = violations.na.drop(subset=['Street Name','House Number','Violation County','Issue Date'])
     violations = violations.select('House Number','Street Name','Violation County', 'Issue Date')
@@ -45,6 +51,13 @@ def clean_violations(violations):
     return(violations_pivot)
 
 def clean_centerline(centerline):
+	'''
+	PREPROCESSING:
+	This function takes in the centerline as the input.
+	The role of this function is to clean the centerline file and split compound house numbers into two column (for each L and R combination)
+	We replace rows that are same in FULL_STREE and ST_LABEL with '0' to avoid the OR operation during join
+	We return: the centerline file after preprocessing
+	'''
     
     centerline = centerline.select('PHYSICALID','L_LOW_HN','L_HIGH_HN', 'R_LOW_HN','R_HIGH_HN','FULL_STREE','ST_LABEL','BOROCODE')
     centerline = centerline.na.drop(subset=['PHYSICALID','L_LOW_HN','L_HIGH_HN', 'R_LOW_HN','R_HIGH_HN','FULL_STREE','ST_LABEL','BOROCODE'])
@@ -74,7 +87,22 @@ def clean_centerline(centerline):
 
 
 def joins(violations, centerline):
-#  TODO first county, street, house number
+    '''
+    This function takes in the pivoted violations and cleaned centerline as input
+    In this function, we perform a total of 8 joins. 4 joins for each condition as mentioned in the problem statement.
+    Times 2 to check for the FULL_STREE and ST_LABEL. This was done to avoid the OR statement.
+    We return dataframes created by joins on:
+
+    (1) CONDITION 1 (FULL_STREE)
+    (2) CONDITION 1 (ST_LABEL)
+    (3) CONDITION 2 (FULL_STREE)
+    (4) CONDITION 2 (ST_LABEL)
+    (5) CONDITION 3 (FULL_STREE)
+    (6) CONDITION 3 (ST_LABEL)
+    (7) CONDITION 4 (FULL_STREE)
+    (8) CONDITION 4 (ST_LABEL)
+    '''
+
     cond1 = [violations['House_Num2'].isNull(),
          violations['House_Num1'] % 2 == 0,
          ((violations['House_Num1'] >= centerline['R_LOW_HN_1']) & (violations['House_Num1'] <= centerline['R_HIGH_HN_1'])),
@@ -89,6 +117,7 @@ def joins(violations, centerline):
          (violations['Street Name'] == centerline['ST_LABEL'])]
     cond1_1_violations = violations.join(centerline.hint("broadcast"), cond1_1, 'right').cache()
 
+    ####
     cond2 = [violations['House_Num2'].isNull(),
          violations['House_Num1'] % 2 == 1,
          ((violations['House_Num1'] >= centerline['L_LOW_HN_1']) & (violations['House_Num1'] <= centerline['L_HIGH_HN_1'])),
@@ -103,6 +132,7 @@ def joins(violations, centerline):
          (violations['Street Name'] == centerline['ST_LABEL'])]
     cond2_2_violations = violations.join(centerline.hint("broadcast"), cond2_2, 'right').cache()
 
+    ####
     cond3 = [violations['House_Num2'].isNotNull(),
          violations['House_Num2'] % 2 == 0,
          ((violations['House_Num2'] >= centerline['R_LOW_HN_2']) & (violations['House_Num2'] <= centerline['R_HIGH_HN_2'])),
@@ -119,6 +149,7 @@ def joins(violations, centerline):
          (violations['Street Name'] == centerline['ST_LABEL'])]
     cond3_3_violations = violations.join(centerline.hint("broadcast"), cond3_3, 'right').cache()
 
+    ####
     cond4 = [violations['House_Num2'].isNotNull(),
          violations['House_Num2'] % 2 == 1,
          ((violations['House_Num2'] >= centerline['L_LOW_HN_2']) & (violations['House_Num2'] <= centerline['L_HIGH_HN_2'])),
@@ -139,12 +170,12 @@ def joins(violations, centerline):
 
     return(cond1_violations,cond1_1_violations,cond2_violations,cond2_2_violations,cond3_violations,cond3_3_violations,cond4_violations,cond4_4_violations)
 
-    print("conditional joins created, moving to the union")
-
-    return(cond1_violations,cond2_violations,cond3_violations,cond4_violations)
-
 
 def unionAll(*dfs):
+	'''
+	This function takes in all the tables created from the "joins" function
+	and returns the union all of the tables. Although UnionAll is deprecated in Spark 2.0.0+, it is still used.
+	'''
     
     print("Union done, we now have the final data set to use")
 
@@ -152,7 +183,10 @@ def unionAll(*dfs):
 
 
 def my_ols(a,b,c,d,e):
-    
+    '''
+    This function takes in the five columns (2015,2016,2017,2018,2019). We use the columns from the pre_ols table.
+    We return: the ols coefficient given every row.
+    '''
     y = ([a,b,c,d,e])
     x = ([2015,2016,2017,2018,2019])
     x = sm.add_constant(x)
@@ -168,10 +202,12 @@ if __name__=='__main__':
     sc = SparkContext().getOrCreate()
     spark = SparkSession(sc)
 
+    # Clearing the cache in hopes that it speeds up the process
     spark.catalog.clearCache()
 
     output_file = sys.argv[1] 
 
+    # Getting the time from the start of the read file(s)
     start = time.time()
     
     violations = spark.read.csv('hdfs:///tmp/bdm/nyc_parking_violation/', 
@@ -186,7 +222,8 @@ if __name__=='__main__':
                     escape ='"',
                     inferSchema = True,
                     multiLine=True).cache()
-
+    
+    # Getting all the distinct physicalID to perform a union at the end to get all the physical IDs 
     centerline_distinct = centerline.select("PHYSICALID").distinct()\
                           .withColumn('2015',F.lit(0))\
                           .withColumn('2016',F.lit(0))\
@@ -196,6 +233,7 @@ if __name__=='__main__':
     
     violations_pivot = clean_violations(violations).cache()
 
+    # The unpersist() function is used throughout this script in order to release dataframes from the cache
     violations.unpersist()
     
     centerline = clean_centerline(centerline)
@@ -213,30 +251,31 @@ if __name__=='__main__':
     cond3_violations.unpersist()
     cond4_violations.unpersist()
 
+    # Here we fill in the values with 0 since these column contain NULL if there was no violations occurred
     result_2 = result.select('PHYSICALID','2015','2016','2017','2018','2019').na.fill(0).orderBy('PHYSICALID').cache()
    
     output_pre_ols = result_2.groupBy('PHYSICALID').sum().cache()
 
     result_2.unpersist()
 
-    temp_result = output_pre_ols.union(centerline_distinct)\
+    # Here we perform a union with the distinct centerline dataframe and pre_ols dataframe to fill in the missing Physical IDs
+    # The resulting dataframe is grouped on "PHYSICALID" and aggregated to get the max of the values.
+    output_pre_ols = output_pre_ols.union(centerline_distinct)\
                 .groupBy("PHYSICALID")
                 .agg(F.max('sum(2015)').alias('2015'),F.max('sum(2016)').alias('2016'), F.max('sum(2017)').alias('2017'), F.max('sum(2018)').alias('2018'), F.max('sum(2019)').alias('2019')).cache()
 
+    # Here, we pass each of the year row as a value to the function to "my_ols", and create a new column "OLS_COEFF" with the output of it
     output_ols = output_pre_ols.withColumn("OLS_COEFF", my_ols(output_pre_ols['sum(2015)'],output_pre_ols['sum(2016)'],output_pre_ols['sum(2017)'],output_pre_ols['sum(2018)'],output_pre_ols['sum(2019)']))\
                                .withColumn("OLS_COEFF", F.round("OLS_COEFF", 3)).cache()
 
 
-
+    # Selecting only the specific columns to make sure no unwanted column is present
     output_ols = output_ols.select('PHYSICALID','2015','2016','2017','2018','2019','OLS_COEFF')
 
-    output_ols = output_ols.withColumn('2015', F.floor(F.col('2015')/2)).withColumn('2016', F.floor(F.col('2016')/2)).withColumn('2017',F.floor(F.col('2017')/2)).withColumn('2018', F.floor(F.col('2018')/2)).withColumn('2019', F.floor(F.col('2019')/2))
+    # output_ols.show()
 
-
-    output_ols.show()
-    
     output_ols.write.csv(output_file, mode = 'overwrite')
 
-
     end = time.time()
-    print(end-start)
+    
+    print(start-end)
